d2898b123415547c46b5844418630d4ba68adc7c
Ivan Potapenko <dreamer4ik@gmail.com>
2022-05-15 18:25:09 +0300

    Initial Commit

---
+++ InstagramSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramSwift
+//
+//  Created by Ivan Potapenko on 15.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
