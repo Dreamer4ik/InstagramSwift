a50f8d4aa335e378b91828a1987327b21177094c
Ivan Potapenko <dreamer4ik@gmail.com>
2022-05-23 18:46:51 +0300

    Filters

--- InstagramSwift/Controllers/Other/CaptionViewController.swift
+++ InstagramSwift/Controllers/Other/CaptionViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class CaptionViewController: UIViewController {
-
+    
+    private let image: UIImage
+    
+    init(image: UIImage) {
+        self.image = image
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .systemBackground
         
     }
     
