3a8fe7cf3dc0c5f0027a46bfc067867e7ed579e8
Ivan Potapenko <dreamer4ik@gmail.com>
2022-05-23 11:17:25 +0300

    Create Custom Camera

--- InstagramSwift/Controllers/Core/CameraViewController.swift
+++ InstagramSwift/Controllers/Core/CameraViewController.swift
@@ -5,15 +5,106 @@
 //  Created by Ivan Potapenko on 15.05.2022.
 //
 
+import AVFoundation
 import UIKit
 
 class CameraViewController: UIViewController {
-
+    
+    private var output = AVCapturePhotoOutput()
+    private var captureSession: AVCaptureSession?
+    private let previewLayer = AVCaptureVideoPreviewLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .black
+        title = "Take Photo"
+        setUpNavBar()
+        checkCameraPermission()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        tabBarController?.tabBar.isHidden = true
+        
+        if let session = captureSession, !session.isRunning {
+            session.startRunning()
+        }
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        captureSession?.stopRunning()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        previewLayer.frame = CGRect(x: 0,
+                                    y: view.safeAreaInsets.top,
+                                    width: view.width,
+                                    height: view.width)
+    }
+    
+    @objc private func didTapClose() {
+        tabBarController?.selectedIndex = 0
+        tabBarController?.tabBar.isHidden = false
+    }
+    
+    private func checkCameraPermission() {
+        
+        switch AVCaptureDevice.authorizationStatus(for: AVMediaType.video) {
+        case .notDetermined:
+            // request
+            AVCaptureDevice.requestAccess(for: .video) { [weak self] granted in
+                guard granted else {
+                    return
+                }
+                DispatchQueue.main.async {
+                    self?.setUpCamera()
+                }
+            }
+        case .authorized:
+            setUpCamera()
+        case .restricted, .denied:
+            break
+        @unknown default:
+            break
+        }
+    }
+    
+    private func setUpCamera() {
+        let captureSession = AVCaptureSession()
+        
+        if let device = AVCaptureDevice.default(for: .video) {
+            do {
+                let input = try AVCaptureDeviceInput(device: device)
+                if captureSession.canAddInput(input) {
+                    captureSession.addInput(input)
+                }
+            }
+            catch {
+                print(error)
+            }
+            
+            if captureSession.canAddOutput(output) {
+                captureSession.canAddOutput(output)
+            }
+            
+            // Layer
+            previewLayer.session = captureSession
+            previewLayer.videoGravity = .resizeAspectFill
+            view.layer.addSublayer(previewLayer)
+            
+            captureSession.startRunning()
+        }
+    }
+    
+    private func setUpNavBar() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .close,
+            target: self,
+            action: #selector(didTapClose)
+        )
         
     }
     
-
 }
